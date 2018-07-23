47e7bcafbd27965359a5f31069eb493f7a36cf28
Mosarrof Hossain <phmosharaf@gmail.com>
2018-07-23 00:20:26 -0400

    Selenium different code

--- src/test/java/seleniumWeb/Selenium1001.java
+++ src/test/java/seleniumWeb/Selenium1001.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium1001 {
-	
-	
 
 	public static void main(String[] args){
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mosarrof Hossain\\Utilities\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://github.com/phmosarrof/SeleniumWebDrive1001");
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		//System.out.println(driver.getPageSource());
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getClass());
 	
 
 	}
